21c840d86ca11da25bec5fa719c08be2ac37ded5
Manu <manuelalberding@gmail.com>
2023-02-05 14:35:44 +0100

    server: feat: manually join tags into document

--- dings_serverpod_server/lib/src/endpoints/dokumento_endpoint.dart
+++ dings_serverpod_server/lib/src/endpoints/dokumento_endpoint.dart
@@ -2,11 +2,40 @@ import 'package:dings_serverpod_server/src/generated/protocol.dart';
 import 'package:serverpod/serverpod.dart';
 
 class DokumentoEndpoint extends Endpoint {
-  Future<List<Dokumento>> getAll(Session session) {
-    return Dokumento.find(session);
+  Future<List<Dokumento>> getAll(Session session) async {
+    final docs = await Dokumento.find(session);
+    for (var doc in docs) {
+      final conTbl = DokumentoTagConnection.t;
+      final tagTbl = DokumentoTag.t;
+      final tags = (await session.db.query("""
+        SELECT ${tagTbl.tableName}.*
+        FROM ${tagTbl.tableName},${conTbl.tableName}
+        WHERE ${tagTbl.tableName}."${tagTbl.id.columnName}" = ${conTbl.tableName}."${conTbl.tagId.columnName}" AND
+        ${conTbl.tableName}."${conTbl.dokumentoId.columnName}" = ${doc.id}
+      """))
+          .map((e) => DokumentoTag(
+                id: e[0],
+                title: e[1],
+                colorR: e[2],
+                colorG: e[3],
+                colorB: e[4],
+              ))
+          .toList();
+      doc.tags = tags;
+    }
+    return docs;
   }
 
-  Future<void> add(Session session, Dokumento doc) {
-    return Dokumento.insert(session, doc);
+  Future<void> add(Session session, Dokumento doc) async {
+    final res = await session.db.transaction((transaction) async {
+      await Dokumento.insert(session, doc);
+      for (var element in doc.tags ?? []) {
+        await DokumentoTagConnection.insert(
+          session,
+          DokumentoTagConnection(tagId: element.id, dokumentoId: doc.id!),
+        );
+      }
+    });
+    return;
   }
 }
